c2b71177357736d367a0e14ff6d062597b257cef
Eman Bathan <90688651+1820121-CPE@users.noreply.github.com>
2023-07-30 16:56:22 +0800

    Add files via upload

---
+++ public_html/ERROR/404.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Incorrect....</title>
+  <link rel="stylesheet" media="all" href="style.css" />
+</head>
+<body>
+<h1>404 Error Page</h1>
+<p class="zoom-area"><b>Try Again</b> You've entered the wrong username or password</p>
+<section class="error-container">
+  <span class="four"><span class="screen-reader-text">4</span></span>
+  <span class="zero"><span class="screen-reader-text">0</span></span>
+  <span class="four"><span class="screen-reader-text">4</span></span>
+</section>
+<div class="link-container">
+  <a href="/login.php" class="more-link">Back to Log In Page</a>
+</div>
+</body>
+</html>
---
+++ public_html/ERROR/4044.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>404 ERROR</title>
+  <link rel="stylesheet" media="all" href="style.css" />
+</head>
+<body>
+<h1>404 Error Page</h1>
+<p class="zoom-area"><b> Try Again</b>The username exists; please choose another!</p>
+<section class="error-container">
+  <span class="four"><span class="screen-reader-text">4</span></span>
+  <span class="zero"><span class="screen-reader-text">0</span></span>
+  <span class="four"><span class="screen-reader-text">4</span></span>
+</section>
+<div class="link-container">
+  <a href="/login.php" class="more-link">Back to Log In Page</a>
+</div>
+</body>
+</html>
